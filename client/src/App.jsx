0ef67732a3bb846f5b7e632533039df4f083ae93
okellogeorge okoth <okoth603@gmail.com>
2024-11-22 18:39:47 +0300

    Creating Hospital Pages and connecting Tailwind Css for Styling

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+       <h1 className="text-red-300 font-bold underline">
+      Hello world!
+    </h1>
+    </div>
+  )
+}
