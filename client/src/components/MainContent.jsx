0bc4efc460c5db6214ad5fddd43dae1c0573f528
okellogeorge okoth <okoth603@gmail.com>
2024-11-23 01:08:31 +0300

    Created Vaccination section for patients

--- client/src/components/MainContent.jsx
+++ client/src/components/MainContent.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types'; // Import PropTypes for validation
-import Inventory from '../pages/Inventory';
+import Vaccination from '../pages/Vaccination';
 
 export default function MainContent({ isSidebarOpen }) {
   return (
@@ -10,7 +10,7 @@ export default function MainContent({ isSidebarOpen }) {
     >
       <div className="text-center">
         <h1 className="text-red-300 font-bold underline">Main Content</h1>
-        <Inventory />
+        <Vaccination />
       </div>
     </div>
   );
