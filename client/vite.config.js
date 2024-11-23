154e3b8607d7ed6319b381fcdd4c2d0d578fc5b3
okellogeorge okoth <okoth603@gmail.com>
2024-11-23 05:29:33 +0300

    Updated root abd vite files

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Uncomment the next line if deploying to GitHub Pages
+  // Replace `your-repo-name` with your actual repository name
+  base: '/okellogeorgeokoth/',
+
+  build: {
+    outDir: 'dist', // Change this if you want a different output folder
+  },
+
+  server: {
+    port: 3000, // Change this if you want a different dev server port
+  },
 })
